7bee58e6b15ff656e17332aaa77c99673f783ceb
DANIEL MOYA FUSTER <danielmoyafuster@gmail.com>
2025-02-01 16:40:23 +0100

    Revisando base de datos

--- pages/1_VerificarFicheros.py
+++ pages/1_VerificarFicheros.py
@@ -1,10 +1,17 @@
 import os
 import streamlit as st
 
-st.write("Archivos en el directorio actual:", os.listdir(os.getcwd()))
-st.write("Base de datos existe:", os.path.exists("./db/FonotecaRadioUMH.db"))
+
+#
+# verificando la base de datos
+#
 
 db_path = "./db/FonotecaRadioUMH.db"
 
+st.write("Archivos en el directorio actual:", os.listdir(os.getcwd()))
 st.write("Archivos en la carpeta 'db':", os.listdir("./db"))
 st.write("Base de datos existe:", os.path.exists(db_path))
+
+
+
+st.write("Permisos del archivo:", os.stat(db_path))
