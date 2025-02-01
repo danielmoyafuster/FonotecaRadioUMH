4d2dccf9e78895f89775e75815c6092d2e95a255
DANIEL MOYA FUSTER <danielmoyafuster@gmail.com>
2025-02-01 16:37:54 +0100

    Verificar base de datos

--- pages/1_VerificarFicheros.py
+++ pages/1_VerificarFicheros.py
@@ -3,3 +3,8 @@ import streamlit as st
 
 st.write("Archivos en el directorio actual:", os.listdir(os.getcwd()))
 st.write("Base de datos existe:", os.path.exists("./db/FonotecaRadioUMH.db"))
+
+db_path = "./db/FonotecaRadioUMH.db"
+
+st.write("Archivos en la carpeta 'db':", os.listdir("./db"))
+st.write("Base de datos existe:", os.path.exists(db_path))
