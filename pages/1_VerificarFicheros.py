a515d8fc39d2451cdb706511755064c91116b2a0
DANIEL MOYA FUSTER <danielmoyafuster@gmail.com>
2025-02-01 16:42:16 +0100

    Verificando base de datos

--- pages/1_VerificarFicheros.py
+++ pages/1_VerificarFicheros.py
@@ -15,3 +15,17 @@ st.write("Base de datos existe:", os.path.exists(db_path))
 
 
 st.write("Permisos del archivo:", os.stat(db_path))
+
+#
+# comprobando integridad de la base de datos
+#
+
+try:
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA integrity_check;")
+    result = cursor.fetchone()
+    st.write("Resultado de PRAGMA integrity_check:", result)
+    conn.close()
+except Exception as e:
+    st.error(f"Error al abrir la base de datos: {e}")
